ed44adc64de2793a80240e0ea7891233655369eb
zzzzeno <zeno.ca@gmail.com>
2026-02-03 13:57:40 +0100

    CFF now supports subgraphs

--- src/utils/cff.py
+++ src/utils/cff.py
@@ -10,6 +10,7 @@ from utils.utils import Colour, np_cmplx_one, np_cmplx_zero
 class EdgeOrientation(StrEnum):
     DEFAULT = "DEFAULT"
     REVERSED = "REVERSED"
+    UNDIRECTED = "UNDIRECTED"
 
     @staticmethod
     def from_str(label: str) -> "EdgeOrientation":
@@ -17,10 +18,15 @@ class EdgeOrientation(StrEnum):
             return EdgeOrientation.DEFAULT
         if label.upper() == "REVERSED":
             return EdgeOrientation.REVERSED
+        if label.upper() == "UNDIRECTED":
+            return EdgeOrientation.UNDIRECTED
         raise ValueError(f"Unknown EdgeOrientation: {label}")
 
     def is_reversed(self) -> bool:
         return self == EdgeOrientation.REVERSED
+
+    def is_undirected(self) -> bool:
+        return self == EdgeOrientation.UNDIRECTED
 
     def __str__(self) -> str:
         match self:
@@ -28,14 +34,35 @@ class EdgeOrientation(StrEnum):
                 return f"{Colour.GREEN}+{Colour.END}"
             case EdgeOrientation.REVERSED:
                 return f"{Colour.RED}-{Colour.END}"
+            case EdgeOrientation.UNDIRECTED:
+                return f"{Colour.BLUE}-{Colour.BLUE}"
 
 
 class CFFTerm(object):
-    def __init__(self, id: int, orientation: tuple[EdgeOrientation, ...], expression: Expression, families: tuple[tuple[bool, ...], ...]):
+    def __init__(
+        self,
+        id: int,
+        orientation: tuple[EdgeOrientation, ...],
+        expression: Expression,
+        families: tuple[tuple[bool, ...], ...],
+    ):
         self.id = id
         self.orientation = orientation
         self.orientation_signs = numpy.zeros(len(orientation), complex)
-        self.orientation_signs[:] = [-np_cmplx_one if o.is_reversed() else np_cmplx_one for o in orientation]
+
+        vals = []
+        for o in orientation:
+            if o.is_reversed():
+                vals.append(-np_cmplx_one)
+            elif o.is_undirected():
+                vals.append(np_cmplx_zero)
+            else:
+                vals.append(np_cmplx_one)
+        self.orientation_signs[:] = vals
+
+        # self.orientation_signs[:] = [
+        #    -np_cmplx_one if o.is_reversed() else np_cmplx_one for o in orientation
+        # ]
         self.expression = expression
         self.families = families
         self.masks = []
@@ -45,15 +72,24 @@ class CFFTerm(object):
             self.masks.append(family_mask)
 
     def __str__(self, show_families=False):
-        res = [f"{''.join(str(o) for o in self.orientation)}: {self.expression.format(show_namespaces=False)}"]
+        res = [
+            f"{''.join(str(o) for o in self.orientation)}: {self.expression.format(show_namespaces=False)}"
+        ]
         if show_families:
             for cff_family in self.families:
-                res.append(f"      {''.join('■' if included else '□' for included in cff_family)}")
+                res.append(
+                    f"      {''.join('■' if included else '□' for included in cff_family)}"
+                )
         return "\n".join(res)
 
 
 class ESurface(object):
-    def __init__(self, id: int, oses: tuple[int, ...], external_shift: tuple[tuple[int, int], ...]):
+    def __init__(
+        self,
+        id: int,
+        oses: tuple[int, ...],
+        external_shift: tuple[tuple[int, int], ...],
+    ):
         self.id = id
         self.oses = oses
         self.external_shift = external_shift
@@ -71,12 +107,37 @@ class ESurface(object):
         return self.expression.format(show_namespaces=False)
 
 
+class HSurface(object):
+    def __init__(
+        self,
+        id: int,
+        positive_oses: tuple[int, ...],
+        negative_oses: tuple[int, ...],
+    ):
+        self.id = id
+        self.positive_oses = positive_oses
+        self.negative_oses = negative_oses
+        self.expression: Expression = self.get_expression()
+
+    def get_expression(self) -> Expression:
+        h_surf = E("0")
+        for e_id in self.positive_oses:
+            h_surf += CFFStructure.SB["E"](e_id)
+        for e_id in self.negative_oses:
+            h_surf -= CFFStructure.SB["E"](e_id)
+        return h_surf
+
+    def __str__(self):
+        return self.expression.format(show_namespaces=False)
+
+
 class CFFStructure(object):
     # fmt: off
     SB = {
         "E": S("pygloop::E"),
         "p0": S("pygloop::E"),
         "eta": S("pygloop::η"),
+        "heta": S("pygloop::γ"),
         "x_": S("pygloop::x_"),
         "eta(x_)": E("η(x_)", default_namespace="pygloop"),
         "x__+y__": E("x__ + y__", default_namespace="pygloop")
@@ -86,6 +147,7 @@ class CFFStructure(object):
     def __init__(self, cff_dict: dict):
         self.cff_dict = cff_dict
         self.e_surfaces: list[ESurface] = []
+        self.h_surfaces: list[HSurface] = []
         self.expressions: list[CFFTerm] = []
         self.build_cff_expression()
 
@@ -95,31 +157,63 @@ class CFFStructure(object):
         for e_surf in self.e_surfaces:
             res.append(f" {Colour.BLUE}#{e_surf.id:-3}{Colour.END} > {str(e_surf)}")
         res.append("")
+        res.append(f"{Colour.GREEN}{len(self.h_surfaces)}{Colour.END} h-surfaces:")
+        for h_surf in self.h_surfaces:
+            res.append(f" {Colour.BLUE}#{e_surf.id:-3}{Colour.END} > {str(h_surf)}")
+        res.append("")
         res.append(f"{Colour.GREEN}{len(self.expressions)}{Colour.END} orientations:")
         for cff_expr in self.expressions:
-            res.append(f" {Colour.BLUE}#{cff_expr.id:-3}{Colour.END} > {cff_expr.__str__(show_families=show_families)}")
+            res.append(
+                f" {Colour.BLUE}#{cff_expr.id:-3}{Colour.END} > {cff_expr.__str__(show_families=show_families)}"
+            )
         res.append("")
         return "\n".join(res)
 
     @classmethod
-    def expression_from_node(cls, node_id: int, nodes_list: list[dict]) -> tuple[Expression, Expression]:
-        eta = cls.SB["eta"](nodes_list[node_id]["data"]["Esurface"])
+    def expression_from_node(
+        cls, node_id: int, nodes_list: list[dict]
+    ) -> tuple[Expression, Expression]:
+        iteration_list = nodes_list[node_id]["data"]
+        if "Hsurface" in iteration_list:
+            eta = cls.SB["heta"](nodes_list[node_id]["data"]["Hsurface"])
+        if "Esurface" in iteration_list:
+            eta = cls.SB["eta"](nodes_list[node_id]["data"]["Esurface"])
         if len(nodes_list[node_id]["children"]) == 0:
             return (eta, 1 / eta)
         children_expression = E("0")
         children_expression_inv = E("0")
         for child in nodes_list[node_id]["children"]:
-            child_expression, child_expression_inv = CFFStructure.expression_from_node(child, nodes_list)
+            child_expression, child_expression_inv = CFFStructure.expression_from_node(
+                child, nodes_list
+            )
             children_expression += child_expression
             children_expression_inv += child_expression_inv
         return (eta * children_expression, (1 / eta) * children_expression_inv)
 
     def build_cff_expression(self):
         for e_id, e_surf in enumerate(self.cff_dict["surfaces"]["esurface_cache"]):
-            self.e_surfaces.append(ESurface(id=e_id, oses=e_surf["energies"], external_shift=e_surf["external_shift"]))
+            self.e_surfaces.append(
+                ESurface(
+                    id=e_id,
+                    oses=e_surf["energies"],
+                    external_shift=e_surf["external_shift"],
+                )
+            )
 
+        for e_id, h_surf in enumerate(self.cff_dict["surfaces"]["hsurface_cache"]):
+            self.h_surfaces.append(
+                HSurface(
+                    id=e_id,
+                    positive_oses=h_surf["positive_energies"],
+                    negative_oses=h_surf["negative_energies"],
+                )
+            )
+
+        print(self.cff_dict["orientations"])
+        print("now inside loop")
         for o_id, o_info in enumerate(self.cff_dict["orientations"]):
             nodes = o_info["expression"]["nodes"]
+            print(o_info)
             o_expression, o_expression_inv = CFFStructure.expression_from_node(0, nodes)
 
             o_families = []
@@ -130,14 +224,22 @@ class CFFStructure(object):
             else:
                 cff_terms = [t for t in expanded_cff_expression]
             for cff_family in cff_terms:
-                eta_ids = [int(str(m[self.SB["x_"]])) for m in cff_family.match(self.SB["eta(x_)"])]
-                mask = tuple(eta_id in eta_ids for eta_id in range(len(self.e_surfaces)))
+                eta_ids = [
+                    int(str(m[self.SB["x_"]]))
+                    for m in cff_family.match(self.SB["eta(x_)"])
+                ]
+                mask = tuple(
+                    eta_id in eta_ids for eta_id in range(len(self.e_surfaces))
+                )
                 o_families.append(mask)
 
             self.expressions.append(
                 CFFTerm(
                     id=o_id,
-                    orientation=tuple(EdgeOrientation.from_str(d) for d in o_info["data"]["orientation"]),
+                    orientation=tuple(
+                        EdgeOrientation.from_str(d)
+                        for d in o_info["data"]["orientation"]
+                    ),
                     expression=o_expression_inv,
                     families=tuple(o_families),
                 )
